e84841b26fb0140b4c83de86f26b07c2b7279036
Mateus Marçal <moteus10@gmail.com>
2022-06-20 16:22:43 -0300

    Feat: add Routes
    Create home page route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" render={ () => (<Home />) } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
