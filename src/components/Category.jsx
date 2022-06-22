8c657302f04b581817defa2b8a2c5cf91a5695f4
Vinicius de Medeiros <vmeedeiros@gmail.com>
2022-06-22 18:28:53 -0300

    Requisito 29 e 30

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import ReceitasContext from '../context/ReceitasContext';
 
 function Category({ returnAPI, pathname }) {
-  const { setUrlAPI } = useContext(ReceitasContext);
+  const { urlAPI, setUrlAPI } = useContext(ReceitasContext);
   const values = Object.values(returnAPI)[0];
   const FIVE = 5;
   const endpointList = [
@@ -11,13 +11,28 @@ function Category({ returnAPI, pathname }) {
     'https://www.themealdb.com/api/json/v1/1/filter.php?c=',
   ];
 
+  const defaultEndpoint = [
+    'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=',
+    'https://www.themealdb.com/api/json/v1/1/search.php?s=',
+  ];
+
   const selectedCategory = ({ target }) => {
     console.log('entrou selected');
     const { value } = target;
     if (pathname === '/drinks') {
-      setUrlAPI(endpointList[0] + value);
+      const urlFilter = endpointList[0] + value;
+      if (urlFilter === urlAPI) {
+        setUrlAPI(defaultEndpoint[0]);
+      } else {
+        setUrlAPI(endpointList[0] + value);
+      }
     } else {
-      setUrlAPI(endpointList[1] + value);
+      const urlFilter = endpointList[1] + value;
+      if (urlFilter === urlAPI) {
+        setUrlAPI(defaultEndpoint[1]);
+      } else {
+        setUrlAPI(endpointList[1] + value);
+      }
     }
   };
   return (
