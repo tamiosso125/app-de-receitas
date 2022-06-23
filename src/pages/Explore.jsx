97a6e9e2de23ba7c04e7e4abbd0100b6f6bfb414
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-06-23 15:19:58 -0300

    Requisitos 67, 68 e 69, concluidos. Redirecionando para as telas de explorar respectivas

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import titleGenerator from '../services/titleGenerator';
@@ -9,6 +10,22 @@ function Explore(props) {
   return (
     <>
       <Header title={ titleGenerator(pathname) } buttonProfile />
+      <Link to="/explore/foods">
+        <button
+          type="button"
+          data-testid="explore-foods"
+        >
+          Explore Foods
+        </button>
+      </Link>
+      <Link to="/explore/drinks">
+        <button
+          type="button"
+          data-testid="explore-drinks"
+        >
+          Explore Drinks
+        </button>
+      </Link>
       <Footer />
     </>
   );
