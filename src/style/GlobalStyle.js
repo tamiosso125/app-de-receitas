b770033508290a4b93c6ab2db1fa11698791b36d
Mateus Marçal <moteus10@gmail.com>
2022-06-29 16:19:05 -0300

    Feat: Create Global Style
    Create Global style for set font-family

---
+++ src/style/GlobalStyle.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Source Sans Pro', sans-serif;
+    background-color: #F0F5F9;
+  }
+`;
+
+export default GlobalStyle;
