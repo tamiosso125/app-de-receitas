cf48f4e1ea6cb9aeda0b22d886798c289205ba30
Mateus Marçal <moteus10@gmail.com>
2022-06-29 17:48:49 -0300

    Style: Button Profile

---
+++ src/components/ButtonProfile/ButtonProfile.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const ProfileButton = styled.button`
+  border-radius: 90%;
+  margin-left: 20px;
+  width: 50px;
+  height: 50px;
+  background-color: white;
+`;
+
+export const ProfileImage = styled.img`
+  
+`;
--- src/components/ButtonProfile/index.jsx
+++ src/components/ButtonProfile/index.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import ProfileIcon from '../images/profileIcon.svg';
+import ProfileIcon from '../../images/profileIcon.svg';
+
+import { ProfileButton, ProfileImage } from './ButtonProfile.styled';
 
 function ButtonProfile() {
   return (
     <Link to="/profile">
-      <button
+      <ProfileButton
         type="button"
         data-testid="profile-top-btn"
         src={ ProfileIcon }
       >
-        <img src={ ProfileIcon } alt="profile" />
-      </button>
+        <ProfileImage src={ ProfileIcon } alt="profile" />
+      </ProfileButton>
     </Link>
   );
 }
